70027ad39b323631e099b575e1aef879edae62dd
Fran Zekan <zekan.fran369@gmail.com>
2023-01-24 21:28:29 +0100

    Simplify api

--- server/main.ts
+++ server/main.ts
@@ -53,11 +53,11 @@ server.post(
       return reply.code(401).send("Invalid password");
     }
 
-    return sign({ id: user.id }, process.env["SECRET"]!);
+    return { id: user.id, token: sign({ id: user.id }, process.env["SECRET"]!) };
   }
 );
 
-const validateUser = (request: FastifyRequest) => {
+const validateUser = async (request: FastifyRequest) => {
   const token = request.headers?.authorization?.split(" ")?.[1];
   if (!token) {
     return [false, "Unauthorized"] as const;
@@ -65,56 +65,52 @@ const validateUser = (request: FastifyRequest) => {
 
   try {
     const decoded = verify(token, process.env["SECRET"]!) as { id: string };
-    // @ts-ignore
-    if (decoded.id !== request.params.id) {
-      return [false, "Wrong user"] as const;
+    if (!decoded.id) {
+      return [false, "Invalid token"] as const;
     }
+
+    const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+    if (!user) {
+      return [false, "User not found"] as const;
+    }
+
+    return [true, user] as const;
   } catch (err) {
     return [false, "Invalid token"] as const;
   }
-
-  return [true, null] as const;
 };
 
-server.post(
-  "/syncs/:id",
-  { schema: { params: Type.Object({ id: Type.String() }) } },
-  (request, reply) => {
-    const [ok, error] = validateUser(request);
-    if (!ok) {
-      return reply.send(error);
-    }
+server.post("/sync", async (request, reply) => {
+  const [ok, data] = await validateUser(request);
+  if (!ok) {
+    return reply.send(data);
+  }
 
-    return prisma.sync.create({
-      data: {
-        data: (request.body as any)["data"],
-        user: {
-          connect: {
-            id: request.params.id,
-          },
+  return prisma.sync.create({
+    data: {
+      data: (request.body as any)["data"],
+      user: {
+        connect: {
+          id: data.id,
         },
       },
-    });
-  }
-);
+    },
+  });
+});
 
-server.get(
-  "/syncs/:id/last",
-  { schema: { params: Type.Object({ id: Type.String() }) } },
-  (request, reply) => {
-    const [ok, error] = validateUser(request);
-    if (!ok) {
-      return reply.send(error);
-    }
-
-    return prisma.sync.findFirst({
-      where: { userId: request.params.id },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+server.get("/sync", async (request, reply) => {
+  const [ok, data] = await validateUser(request);
+  if (!ok) {
+    return reply.send(data);
   }
-);
+
+  return prisma.sync.findFirst({
+    where: { userId: data.id },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+});
 
 (async () => {
   let port = 3000;
