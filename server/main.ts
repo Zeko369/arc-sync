b9e22a96d80d23830e7c19f9bf9012321bb6736b
Fran Zekan <zekan.fran369@gmail.com>
2023-01-24 21:01:51 +0100

    Arc sync server

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,9 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
 import { PrismaClient } from "@prisma/client";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import { Type } from "@sinclair/typebox";
+import { compare } from "bcryptjs";
+import { sign, verify } from "jsonwebtoken";
 
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 const prisma = new PrismaClient();
@@ -10,30 +12,57 @@ server.get("/", (request, reply) => {
   reply.send("Hello there!");
 });
 
-server.get("/users", (req, reply) => prisma.user.findMany());
 server.post(
-  "/users",
-  { schema: { body: Type.Object({ email: Type.String() }) } },
-  async (req, reply) => {
-    const user = await prisma.user.create({ data: { email: req.body.email } });
-    return user;
+  "/login",
+  { schema: { body: Type.Object({ email: Type.String(), password: Type.String() }) } },
+  async (request, reply) => {
+    const user = await prisma.user.findUnique({ where: { email: request.body.email } });
+    if (!user) {
+      return reply.code(401).send("User not found");
+    }
+
+    if (await compare(request.body.password, user?.hashedPassword)) {
+      return reply.code(401).send("Invalid password");
+    }
+
+    return sign({ id: user.id }, process.env["SECRET"]!);
   }
 );
 
+const validateUser = (request: FastifyRequest) => {
+  const token = request.headers?.authorization?.split(" ")?.[1];
+  if (!token) {
+    return [false, "Unauthorized"] as const;
+  }
+
+  try {
+    const decoded = verify(token, process.env["SECRET"]!) as { id: string };
+    // @ts-ignore
+    if (decoded.id !== request.params.id) {
+      return [false, "Wrong user"] as const;
+    }
+  } catch (err) {
+    return [false, "Invalid token"] as const;
+  }
+
+  return [true, null] as const;
+};
+
 server.post(
   "/syncs/:id",
-  {
-    schema: {
-      params: Type.Object({ id: Type.String() }),
-    },
-  },
-  (req, reply) => {
+  { schema: { params: Type.Object({ id: Type.String() }) } },
+  (request, reply) => {
+    const [ok, error] = validateUser(request);
+    if (!ok) {
+      return reply.send(error);
+    }
+
     return prisma.sync.create({
       data: {
-        data: (req.body as any)["data"],
+        data: (request.body as any)["data"],
         user: {
           connect: {
-            id: req.params.id,
+            id: request.params.id,
           },
         },
       },
@@ -44,9 +73,14 @@ server.post(
 server.get(
   "/syncs/:id",
   { schema: { params: Type.Object({ id: Type.String() }) } },
-  (req, reply) => {
+  (request, reply) => {
+    const [ok, error] = validateUser(request);
+    if (!ok) {
+      return reply.send(error);
+    }
+
     return prisma.sync.findFirst({
-      where: { userId: req.params.id },
+      where: { userId: request.params.id },
       orderBy: {
         createdAt: "desc",
       },
@@ -55,8 +89,13 @@ server.get(
 );
 
 (async () => {
+  let port = 3000;
+  if (process.env["PORT"] && Number.isNaN(Number(process.env["PORT"]))) {
+    port = Number(process.env["PORT"]);
+  }
+
   try {
-    const addr = await server.listen({ port: 3000, host: "0.0.0.0" });
+    const addr = await server.listen({ port, host: "0.0.0.0" });
     console.log(`Server listening on ${addr}`);
   } catch (err) {
     server.log.error(err);
